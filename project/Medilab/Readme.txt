eb9e791c34237c0434033ef388e25b93a5b4e0de
arya2004 <arya20j4@gmail.com>
2023-04-15 22:03:10 +0530

    done for today

--- project/Medilab/Readme.txt
+++
@@ -1,6 +0,0 @@
-Thanks for downloading this template!
-
-Template Name: Medilab
-Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
--- project/app.js
+++ project/app.js
@@ -6,7 +6,23 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const passport = require('passport');
 const localStrategy = require('passport-local');
+const session = require('express-session')
+const flash = require('connect-flash')
 
+const Test = require('./models/test')
+
+
+const sessionConfig = {
+    secret: 'thisshouldbeabetersectere',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + (1000*60*60*24*7),
+        maxAge: (1000*60*60*24*7)
+    }
+
+}
 
 
 
@@ -25,11 +41,22 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 app.engine('ejs', ejsMate)
 
-//app.use(session(sessionConfig))
-//app.use(flash())
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(session(sessionConfig))
+app.use(flash())
+ app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new localStrategy(Test.authenticate()))
 
+passport.serializeUser(Test.serializeUser())
+passport.deserializeUser(Test.deserializeUser())
+
+//universal middleware
+app.use((req,res,next) =>{
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.get('/', (req,res)=>{
     res.render('index.ejs')
@@ -49,9 +76,31 @@ app.get('/new', (req,res)=>{
     res.render('new.ejs')
     
 })
+app.get('/login', (req,res)=>{
+    res.render('login.ejs')
+    
+})
 
 app.get('/test', (req,res)=>{
     res.render('test.ejs')
+    
+})
+
+
+app.post('/new',async(req,res)=>{
+try{
+    const{username, email, password} = req.body;
+    const test = new Test({username, email});
+    const registeredTest = await Test.register(test,password)
+    res.redirect('/')
+}catch(e){
+    res.redirect('/')
+}
+
+})
+
+app.post('/login', passport.authenticate('local'), (req,res)=>{
+    res.redirect('/')
     
 })
 
@@ -62,6 +111,11 @@ app.post('/', (req,res)=>{
     
     
 })
+
+app.get("*",(req,res)=>{
+    res.render('index.ejs')
+})
+
 app.listen(3000,()=>{
     console.log('sucsex')
 })
